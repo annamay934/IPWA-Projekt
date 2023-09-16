50acdf1809948c7ac268873931281db0ac4e8b57
anna934 <anna.victoriamay@web.de>
2023-09-16 16:40:41 +0200

    change contactpage

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,6 +12,11 @@ function ContactPage() {
           abgeholt werden.
         </p>
 
+        <section>
+          <h2 className="custom-h2">Schreiben Sie uns!</h2>
+          <p>Wir freuen uns auf ihre Nachricht.</p>
+        </section>
+
         <div bordered hover className="table-container">
           <table>
             <thead>
@@ -33,58 +38,6 @@ function ContactPage() {
             </tbody>
           </table>
         </div>
-
-        <section>
-          <h2 className="custom-h2">Unser Engagement für die Umwelt:</h2>
-          <p>
-            Bei unserer Kleiderspendenaktion geht es nicht nur darum, Menschen
-            in Krisengebieten zu helfen, sondern auch die Umwelt zu schonen.
-            Indem Sie Ihre Kleidung bei uns spenden, tragen Sie dazu bei, den
-            Lebenszyklus von Kleidungsstücken zu verlängern und die
-            Ressourcenverschwendung zu reduzieren. Anstatt Kleidung wegzuwerfen,
-            geben Sie ihr eine neue Chance und minimieren so den ökologischen
-            Fußabdruck der Modeindustrie.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="custom-h2">Nachhaltige Mode:</h2>
-          <p>
-            Unsere Kleiderspendenaktion geht über die reine Wohltätigkeit
-            hinaus. Wir setzen uns für nachhaltige Mode ein und möchten
-            Bewusstsein für die Umweltauswirkungen der Textilindustrie schaffen.
-            Mithilfe ihrer Spende unterstützen Sie den Gedanken der
-            Kreislaufwirtschaft. Ihre Kleidungsstücke werden an Bedürftige
-            weitergegeben oder, falls nötig, recycelt, um ihre Ressourcen
-            optimal zu nutzen und Abfall zu vermeiden.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="custom-h2">Gemeinschaftliches Engagement:</h2>
-          <p>
-            Unsere Kleiderspendenannahme ist ein Ort des gemeinschaftlichen
-            Engagements. Hier kommen Menschen mit unterschiedlichen
-            Hintergründen und Geschichten zusammen, um etwas Gutes zu tun. Wir
-            schaffen einen Raum, in dem gegenseitige Unterstützung und
-            Solidarität gedeihen können. Wir laden Sie herzlich ein, Teil
-            unserer Gemeinschaft zu werden und gemeinsam einen positiven
-            Einfluss auf die Welt zu haben.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="custom-h2">Vielen Dank für Ihre Unterstützung:</h2>
-          <p>
-            Ohne die großzügige Unterstützung unserer Spenderinnen und Spender
-            wäre unsere Arbeit nicht möglich. Wir möchten Ihnen von Herzen für
-            Ihr Vertrauen und Ihre Bereitschaft, etwas zu bewegen, danken. Jede
-            Spende, sei es eine einzelne Bluse oder eine vollständige Garderobe,
-            ist von unschätzbarem Wert und hat einen echten Einfluss auf das
-            Leben anderer Menschen. Ihre Großzügigkeit und Mitmenschlichkeit
-            sind inspirierend.
-          </p>
-        </section>
       </body>
     </div>
   );
