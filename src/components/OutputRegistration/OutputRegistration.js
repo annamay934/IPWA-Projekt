b27989c122f386c187daa8331fabb8823ef8a2a6
anna934 <anna.victoriamay@web.de>
2023-09-23 16:19:13 +0200

    change outputregistration else block

--- src/components/OutputRegistration/OutputRegistration.js
+++ src/components/OutputRegistration/OutputRegistration.js
@@ -113,83 +113,15 @@ const OutputRegistration = () => {
     );
   } else {
     console.log("Reached the else block");
-    const {
-      firstName,
-      lastName,
-      street,
-      zip,
-      city,
-      clothes2,
-      location2,
-      clothes1,
-      location1,
-    } = state.registrationData;
 
     return (
       <div>
-        <h1 className="custom-h1">Vielen Dank für ihre Spende!</h1>
-        <h3 className="custom-h3">
-          Ihre Registrierung ist damit abgeschlossen.
-        </h3>
-
-        <Table className="table-container">
-          <tbody>
-            <tr>
-              <th>Vorname</th>
-              <td>{firstName}</td>
-            </tr>
-            <tr>
-              <th>Nachname</th>
-              <td>{lastName}</td>
-            </tr>
-            <tr>
-              <th>Straße</th>
-              <td>{street}</td>
-            </tr>
-            <tr>
-              <th>Postleitzahl</th>
-              <td>{zip}</td>
-            </tr>
-            <tr>
-              <th>Wohnort</th>
-              <td>{city}</td>
-            </tr>
-            <tr>
-              <th>Kleiderart</th>
-              <td>{clothes2}</td>
-            </tr>
-            <tr>
-              <th>Krisengebiet</th>
-              <td>{location2}</td>
-            </tr>
-            <tr>
-              <th>Kleiderart</th>
-              <td>{clothes1}</td>
-            </tr>
-            <tr>
-              <th>Krisengebiet</th>
-              <td>{location1}</td>
-            </tr>
-          </tbody>
-        </Table>
+        <section>
+          <h1 className="custom-h1">Ein Problem ist aufgetreten</h1>
+          <p>Diese Seite konnte nicht gefunden werden.</p>
+        </section>
       </div>
     );
-    // console.log("Dummy Data:", state.dummyData);
-
-    // return (
-    //   <div>
-    //     <h2>Example Data shown</h2>
-    //     <div>
-    //       <p>Vorname: {state.dummyData.firstName}</p>
-    //       <p>Nachname: {state.dummyData.lastName}</p>
-    //       <p>Straße: {state.dummyData.street}</p>
-    //       <p>Postleitzahl: {state.dummyData.zip}</p>
-    //       <p>Wohnort: {state.dummyData.city}</p>
-    //       <p>Kleiderart: {state.dummyData.clothes1}</p>
-    //       <p>Krisengebiet: {state.dummyData.location1}</p>
-    //     </div>
-    //   </div>
-    // );
   }
 };
 
